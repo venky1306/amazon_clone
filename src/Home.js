f034e61686aa25426e098ffba575a908dd00df2b
venky1306 <47674519+venky1306@users.noreply.github.com>
2023-01-03 02:45:47 -0500

    Added Header, and Home components to application

---
+++ src/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./Home.css";
+
+function Home(){
+    return (
+    <div className="home">
+        <div className="home__container">
+        <img
+          className="home__image"
+          src='https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/71egXL5SDEL._SX3000_.jpg'
+          alt=""
+        />
+      </div>
+    </div>
+    );
+}
+
+export default Home;
